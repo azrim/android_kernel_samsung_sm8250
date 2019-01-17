e3f1784032dff3d8b87bc76658db6e4e55b43e3a
Jonathan Avila <avilaj@codeaurora.org>
2019-01-16 20:28:27 -0800

    PM / devfreq: Introduce an event lock
    Currently, concurrent writes to sysfs entries leave the	possibility
    for race conditions within the devfreq framework.  For example,
    concurrently executing max_freq_store and governor_store can result
    in attempting to perform an update_devfreq() before the new governor's
    start handler can be executed.
    
    A more concrete case is a race between polling_interval_store and
    governor_store.  Because no lock is used after calling into the event
    handler of the old governor and there's nothing preventing work from
    being queued after the monitor is stopped, it's possible to
    accidentally cause delayed work to be queued on the governor being
    switched to.  This can be seen if you create two threads, one which
    changes a device's governor between simple_ondemand and performance,
    and one which changes its polling interval between 45 and 50.
    
    All of these races can be addressed with the introduction of a lock
    that prevents sysfs operations from interleaving in this fashion.
    
    Change-Id: Ia6887dcb2d69dc2576837a6c09fed55a28943abc
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    [avajid@codeaurora.org: renamed to event lock and only used when CONFIG_QCOM_DEVFREQ_ICC is enabled]
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    (cherry picked from commit 3f6bc9743e74f567a18b8748df654cb5ea641b4d)

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -603,6 +603,7 @@ static void devfreq_dev_release(struct device *dev)
 		devfreq->profile->exit(devfreq->dev.parent);
 
 	mutex_destroy(&devfreq->lock);
+	event_mutex_destroy(devfreq);
 	srcu_cleanup_notifier_head(&devfreq->transition_notifier_list);
 	kfree(devfreq);
 }
@@ -647,6 +648,7 @@ struct devfreq *devfreq_add_device(struct device *dev,
 	}
 
 	mutex_init(&devfreq->lock);
+	event_mutex_init(devfreq);
 	mutex_lock(&devfreq->lock);
 	devfreq->dev.parent = dev;
 	devfreq->dev.class = devfreq_class;
@@ -1168,12 +1170,13 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 		goto out;
 	}
 
+	event_mutex_lock(df);
 	if (df->governor) {
 		ret = df->governor->event_handler(df, DEVFREQ_GOV_STOP, NULL);
 		if (ret) {
 			dev_warn(dev, "%s: Governor %s not stopped(%d)\n",
 				 __func__, df->governor->name, ret);
-			goto out;
+			goto gov_stop_out;
 		}
 	}
 	prev_governor = df->governor;
@@ -1194,6 +1197,9 @@ static ssize_t governor_store(struct device *dev, struct device_attribute *attr,
 			df->governor = NULL;
 		}
 	}
+
+gov_stop_out:
+	event_mutex_unlock(df);
 out:
 	mutex_unlock(&devfreq_list_lock);
 
@@ -1288,8 +1294,10 @@ static ssize_t polling_interval_store(struct device *dev,
 	if (ret != 1)
 		return -EINVAL;
 
+	event_mutex_lock(df);
 	df->governor->event_handler(df, DEVFREQ_GOV_INTERVAL, &value);
 	ret = count;
+	event_mutex_unlock(df);
 
 	return ret;
 }
@@ -1306,6 +1314,7 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	if (ret != 1)
 		return -EINVAL;
 
+	event_mutex_lock(df);
 	mutex_lock(&df->lock);
 
 	if (value) {
@@ -1328,6 +1337,7 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
+	event_mutex_unlock(df);
 	return ret;
 }
 
@@ -1350,6 +1360,7 @@ static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 	if (ret != 1)
 		return -EINVAL;
 
+	event_mutex_lock(df);
 	mutex_lock(&df->lock);
 
 	if (value) {
@@ -1372,6 +1383,7 @@ static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 	ret = count;
 unlock:
 	mutex_unlock(&df->lock);
+	event_mutex_unlock(df);
 	return ret;
 }
 static DEVICE_ATTR_RW(min_freq);
