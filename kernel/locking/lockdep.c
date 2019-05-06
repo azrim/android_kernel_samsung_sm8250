f5474d659bae88fa2bfc4d4359894ca06dfb2c45
Yuyang Du <duyuyang@gmail.com>
2019-05-06 16:19:36 +0800

    UPSTREAM: locking/lockdep: Check redundant dependency only when CONFIG_LOCKDEP_SMALL
    As Peter has put it all sound and complete for the cause, I simply quote:
    
    "It (check_redundant) was added for cross-release (which has since been
    reverted) which would generate a lot of redundant links (IIRC) but
    having it makes the reports more convoluted -- basically, if we had an
    A-B-C relation, then A-C will not be added to the graph because it is
    already covered. This then means any report will include B, even though
    a shorter cycle might have been possible."
    
    This would increase the number of direct dependencies. For a simple workload
    (make clean; reboot; make vmlinux -j8), the data looks like this:
    
     CONFIG_LOCKDEP_SMALL: direct dependencies:                  6926
    
    !CONFIG_LOCKDEP_SMALL: direct dependencies:                  9052    (+30.7%)
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Yuyang Du <duyuyang@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: bvanassche@acm.org
    Cc: frederic@kernel.org
    Cc: ming.lei@redhat.com
    Cc: will.deacon@arm.com
    Link: https://lkml.kernel.org/r/20190506081939.74287-21-duyuyang@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: I3c04235e309e093ee064edf1178ce6ff533d9267
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -1449,6 +1449,7 @@ check_noncircular(struct held_lock *src, struct held_lock *target,
 	return ret;
 }
 
+#ifdef CONFIG_LOCKDEP_SMALL
 /*
  * Check that the dependency graph starting at <src> can lead to
  * <target> or not. If it can, <src> -> <target> dependency is already
@@ -1478,6 +1479,7 @@ check_redundant(struct held_lock *src, struct held_lock *target)
 
 	return ret;
 }
+#endif
 
 #ifdef CONFIG_TRACE_IRQFLAGS
 
@@ -2121,12 +2123,14 @@ check_prev_add(struct task_struct *curr, struct held_lock *prev,
 		}
 	}
 
+#ifdef CONFIG_LOCKDEP_SMALL
 	/*
 	 * Is the <prev> -> <next> link redundant?
 	 */
 	ret = check_redundant(prev, next);
 	if (ret != 1)
 		return ret;
+#endif
 
 	if (!trace->nr_entries && !save_trace(trace))
 		return 0;
