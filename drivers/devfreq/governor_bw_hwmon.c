3d118d1f8565a87a4471139dad08e13d4064a296
Amir Vajid <avajid@codeaurora.org>
2020-06-30 15:47:25 -0700

    PM / devfreq: make memlat and bwmon governors immutable
    The devfreq devices that support the mem_latency, compute
    or bw_hwmon governors are tightly coupled with hardware
    monitors and are not expected to change governors at
    runtime in normal operation. Thus, make these governors
    immutable and have them start with min_freq equal to
    max_freq to optimize performance during boot. Also make
    the initial polling interval 500 ms to reduce polling
    activity during boot.
    
    Change-Id: Ida67f404155dce01facdfe999d8e71d9e1ac317c
    Signed-off-by: Amir Vajid <avajid@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    (cherry picked from commit 9ac15be3ca5ec778cb054a762cfdbfe593cf3aff)

--- drivers/devfreq/governor_bw_hwmon.c
+++ drivers/devfreq/governor_bw_hwmon.c
@@ -634,6 +634,10 @@ static int gov_start(struct devfreq *df)
 	if (ret)
 		goto err_sysfs;
 
+	mutex_lock(&df->lock);
+	df->min_freq = df->max_freq;
+	update_devfreq(df);
+	mutex_unlock(&df->lock);
 	return 0;
 
 err_sysfs:
