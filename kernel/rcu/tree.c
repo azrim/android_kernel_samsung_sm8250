8d30ee1d7a742dca4142440be9b156ed2c1e069e
Borislav Petkov <bp@suse.de>
2020-09-21 12:31:36 +0200

    BACKPORT: rcu/tree: Export rcu_idle_{enter,exit} to modules
    Fix this link error:
    
      ERROR: modpost: "rcu_idle_enter" [drivers/acpi/processor.ko] undefined!
      ERROR: modpost: "rcu_idle_exit" [drivers/acpi/processor.ko] undefined!
    
    when CONFIG_ACPI_PROCESSOR is built as module. PeterZ says that in light
    of ARM needing those soon too, they should simply be exported.
    
    Link: https://lore.kernel.org/linux-pm/20200921103741.GC5901@zn.tnic/
    Bug: 172034763
    Change-Id: I26d516d11edbce673f52df6474f6695c5a1e2b07
    (cherry picked from commit 3ad1c8ef083bef96ec922688966484be1039e6b5)
    Fixes: 1fecfdbb7acc ("ACPI: processor: Take over RCU-idle for C3-BM idle")
    Reported-by: Sven Joachim <svenjoac@gmx.de>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Paul E. McKenney <paulmckrcu@kernel.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Cheng Jui Wang <cheng-jui.wang@mediatek.com>
    Signed-off-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    Signed-off-by: Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -759,6 +759,7 @@ void rcu_idle_enter(void)
 	lockdep_assert_irqs_disabled();
 	rcu_eqs_enter(false);
 }
+EXPORT_SYMBOL_GPL(rcu_idle_enter);
 
 #ifdef CONFIG_NO_HZ_FULL
 /**
@@ -934,6 +935,7 @@ void rcu_idle_exit(void)
 	rcu_eqs_exit(false);
 	local_irq_restore(flags);
 }
+EXPORT_SYMBOL_GPL(rcu_idle_exit);
 
 #ifdef CONFIG_NO_HZ_FULL
 /**
