875c50a34eaac5004e0d0965a7df082d04de27b4
Sergey Senozhatsky <senozhatsky@chromium.org>
2022-08-24 12:51:00 +0900

    BACKPORT: drivers/block/zram/zram_drv.c: do not keep dangling zcomp pointer after zram reset
    We do all reset operations under write lock, so we don't need to save
    ->disksize and ->comp to stack variables.  Another thing is that ->comp is
    freed during zram reset, but comp pointer is not NULL-ed, so zram keeps
    the freed pointer value.
    
    Link: https://lkml.kernel.org/r/20220824035100.971816-1-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -3232,9 +3232,6 @@ out:
 
 static void zram_reset_device(struct zram *zram)
 {
-	struct zcomp *comp;
-	u64 disksize;
-
 	down_write(&zram->init_lock);
 
 	cancel_delayed_work_sync(&zram->compact_work);
@@ -3245,18 +3242,16 @@ static void zram_reset_device(struct zram *zram)
 		return;
 	}
 
-	comp = zram->comp;
-	disksize = zram->disksize;
-	zram->disksize = 0;
-
 	set_capacity(zram->disk, 0);
 	part_stat_set_all(&zram->disk->part0, 0);
 
 	up_write(&zram->init_lock);
 	/* I/O operation under all of CPU are done so let's free */
-	zram_meta_free(zram, disksize);
+	zram_meta_free(zram, zram->disksize);
+	zram->disksize = 0;
 	memset(&zram->stats, 0, sizeof(zram->stats));
-	zcomp_destroy(comp);
+	zcomp_destroy(zram->comp);
+	zram->comp = NULL;
 	reset_bdev(zram);
 }
 
