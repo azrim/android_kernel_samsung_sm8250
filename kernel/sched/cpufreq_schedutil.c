48a8dbf889e0a6caaaf0988a9646a9f163a4a701
utziacre <utziacre@gmail.com>
2023-10-23 23:31:59 +0800

    cpufreq: schedutil: Scale freq for per cpu
    Change-Id: I18d9215e00ac8a53f14af1868e27d0cb587544f9

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -341,6 +341,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
 
+<<<<<<< HEAD
 #ifdef CONFIG_SCHED_FFSI_GLUE
 	struct sugov_cpu *sg_cpu;
 	struct ffsi_class *vessel;
@@ -357,7 +358,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	RV_DECLARE(rv);
 #endif
 
-	freq = map_util_freq(util, freq, max);
+	freq = map_util_freq(util, freq, max, policy->cpu);
 #ifdef CONFIG_SCHED_FFSI_GLUE
 	legacy_freq = freq;
 
