ebb713b954f19ea3bae7123e958de0c5ab389de4
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-06-16 16:44:20 +0530

    scsi: ufs: Enable auto AH8 after scsi host initialization
    Change-Id: I2cd6183f77874d051708564333b3496b49084762
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    (cherry picked from commit c16bfa4574968963b172c550e6a6aa9fc92b9017)

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -10829,6 +10829,12 @@ reinit:
 		pm_runtime_put_sync(hba->dev);
 	}
 
+	/*
+	 * Enable auto hibern8 if supported, after full host and
+	 * device initialization.
+	 */
+	ufshcd_set_auto_hibern8_timer(hba);
+
 out:
 	if (ret && link_retry_count++ < UFS_LINK_SETUP_RETRIES) {
 		dev_err(hba->dev, "%s: error with %d, and will be reset.(%d)\n",
