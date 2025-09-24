532c60de521f58f6f8b4a30e023e213b88195ed2
Atakan <ata.kaner03@gmail.com>
2025-09-24 20:18:06 +0300

    Fixup! "Merge remote-tracking branch 'sm8250/lineage-20' into OneUI"
    The latest merge of "LineageOS/android_kernel_qcom_sm8250" (Commit: c9208cf4537d) broke the code added by Samsung.
    
    This change re-aligns fs/super.c with "LineageOS/android_kernel_samsung_sm8250" and fixes build errors.
    
    Change-Id: I9707ba07a748b2218d584e088cb7b0331d3b702a

--- fs/super.c
+++ fs/super.c
@@ -37,7 +37,6 @@
 #include <linux/lockdep.h>
 #include <linux/user_namespace.h>
 #include <linux/fs_context.h>
-#include <linux/magic.h>
 #include "internal.h"
 
 static int thaw_super_locked(struct super_block *sb);
@@ -978,9 +977,6 @@ int reconfigure_super(struct fs_context *fc)
 		if (force) {
 			sb->s_readonly_remount = 1;
 			smp_wmb();
-
-			if (sb->s_magic == F2FS_SUPER_MAGIC)
-				mnt = ERR_PTR(-EROFS);
 		} else {
 			retval = sb_prepare_remount_readonly(sb);
 			if (retval)
