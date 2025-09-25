23f5111716ca9d1a5ee40b3329e32b28bf564fd7
Atakan <ata.kaner03@gmail.com>
2025-09-25 21:53:48 +0300

    treewide: Reimplement Samsung Freecess
    From G990BXXSFHYG1
    
    Change-Id: I74e4fc1f7488d4642fb4720870a83b9b8d71fbc9

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -34,6 +34,9 @@
 #include "binder_alloc.h"
 #include "binder_trace.h"
 
+#ifdef CONFIG_SAMSUNG_FREECESS
+#include <linux/freecess.h>
+#endif
 struct list_lru binder_alloc_lru;
 
 static DEFINE_MUTEX(binder_alloc_mmap_lock);
@@ -433,6 +436,19 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 
 	/* Pad 0-size buffers so they get assigned unique addresses */
 	size = max(size, sizeof(void *));
+
+#ifdef CONFIG_SAMSUNG_FREECESS
+	if (is_async && (alloc->free_async_space < 3 * size
+		|| alloc->free_async_space < alloc->buffer_size/4)) {
+		struct task_struct *p;
+
+		rcu_read_lock();
+		p = find_task_by_vpid(alloc->pid);
+		rcu_read_unlock();
+		if (p && (thread_group_is_frozen(p) || p->jobctl & JOBCTL_TRAP_FREEZE))
+			binder_report(p, -1, "free_buffer_full", is_async);
+	}
+#endif
 
 	if (is_async && alloc->free_async_space < size) {
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
