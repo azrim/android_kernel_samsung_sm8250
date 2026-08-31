c6e80cf56b8da7adc15e43ec6acc8cb4806114b9
azrim <mirzaspc@gmail.com>
2026-08-31 11:26:29 +0700

    net/ipv4: tcp_bbr: restore bbr_register init function definition
    (cherry picked from commit 1f72b46317064161c57c7748ca4e5f3457230850)
    (cherry picked from commit 5126047b85d675d15b94a0f5a158c4fafb2714f7)

--- net/ipv4/tcp_bbr.c
+++ net/ipv4/tcp_bbr.c
@@ -2382,6 +2382,10 @@ static struct tcp_congestion_ops tcp_bbr_cong_ops __read_mostly = {
 	.set_state	= bbr_set_state,
 };
 
+static int __init bbr_register(void)
+{
+	BUILD_BUG_ON(sizeof(struct bbr) > ICSK_CA_PRIV_SIZE);
+
 	return tcp_register_congestion_control(&tcp_bbr_cong_ops);
 }
 
