9f1f50dcb35e2989e19054d2bc456e2c03061c37
azrim <mirzaspc@gmail.com>
2026-09-01 13:20:54 +0700

    anykernel: restore solvege branding

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel for S20 FE (Snapdragon) by pascua28 @ xda-developers
+kernel.string=Solvege Kernel for Galaxy S20 FE 5G (SM8250) by azrim
 do.devicecheck=1
 do.modules=0
 do.systemless=1
