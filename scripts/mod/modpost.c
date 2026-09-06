be3a6596081b7459c68d20fed21e611179a80e85
azrim <mirzaspc@gmail.com>
2026-09-06 18:20:33 +0700

    r8q: fix build warnings
    (cherry picked from commit 06142ae359a38a98a0c1894ebfa170f7bf67d198)

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -803,9 +803,10 @@ static int match(const char *sym, const char * const pat[])
 
 		/* "*foo*" */
 		if (*p == '*' && *endp == '*') {
+			const char *here;
 			char *bare = NOFAIL(strndup(p + 1, strlen(p) - 2));
-			const char *here = strstr(sym, bare);
 
+			here = strstr(sym, bare);
 			free(bare);
 			if (here != NULL)
 				return 1;
