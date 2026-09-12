a064c3eab6b3111073818652619b4cba08e0d9de
Casey Connolly <casey.connolly@linaro.org>
2026-09-13 04:00:50 +0700

    UPSTREAM: ASoC: detect empty DMI strings
    Some bootloaders like recent versions of U-Boot may install some DMI
    properties with empty values rather than not populate them. This manages
    to make its way through the validator and cleanup resulting in a rogue
    hyphen being appended to the card longname.
    
    Fixes: 4e01e5dbba96 ("ASoC: improve the DMI long card code in asoc-core")
    Signed-off-by: Casey Connolly <casey.connolly@linaro.org>
    Link: https://patch.msgid.link/20260306174707.283071-2-casey.connolly@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit a9683730e8b1d632674f81844ed03ddfbe4821c0)

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1792,12 +1792,15 @@ static void cleanup_dmi_name(char *name)
 	name[j] = '\0';
 }
 
-/* Check if a DMI field is valid, i.e. not containing any string
- * in the black list.
+/ * Check if a DMI field is valid, i.e. not containing any string
+ * in the black list and not the empty string.
  */
 static int is_dmi_valid(const char *field)
 {
 	int i = 0;
+
+	if (!field[0])
+		return 0;
 
 	while (dmi_blacklist[i]) {
 		if (strstr(field, dmi_blacklist[i]))
