cf15181ddd14e1d3ec7b01779c3637873e52f585
Gao Xiang <hsiangkao@linux.alibaba.com>
2026-09-12 07:50:50 +0700

    UPSTREAM: erofs: fix incorrect symlink detection in fast symlink
    Fast symlink can be used if the on-disk symlink data is stored
    in the same block as the on-disk inode, so we don’t need to trigger
    another I/O for symlink data.  However, currently fs correction could be
    reported _incorrectly_ if inode xattrs are too large.
    
    In fact, these should be valid images although they cannot be handled as
    fast symlinks.
    
    [ Note that it's a runtime misbehavior instead of a security issue. ]
    
    (cherry picked from commit 9ed50b8231e37b1ae863f5dec8153b98d9f389b4)
    
    Fixes: 431339ba9042 ("staging: erofs: add inode operations")
    Cc: stable@kernel.org

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -199,9 +199,10 @@ static int erofs_fill_symlink(struct inode *inode, void *data,
 	struct erofs_inode *vi = EROFS_I(inode);
 	char *lnk;
 
+	m_pofs += vi->xattr_isize;
 	/* if it cannot be handled with fast symlink scheme */
 	if (vi->datalayout != EROFS_INODE_FLAT_INLINE ||
-	    inode->i_size >= PAGE_SIZE) {
+	    inode->i_size < 0 || m_pofs + inode->i_size > PAGE_SIZE) {
 		inode->i_op = &erofs_symlink_iops;
 		return 0;
 	}
@@ -209,17 +210,6 @@ static int erofs_fill_symlink(struct inode *inode, void *data,
 	lnk = kmalloc(inode->i_size + 1, GFP_KERNEL);
 	if (!lnk)
 		return -ENOMEM;
-
-	m_pofs += vi->xattr_isize;
-	/* inline symlink data shouldn't cross page boundary as well */
-	if (m_pofs + inode->i_size > PAGE_SIZE) {
-		kfree(lnk);
-		erofs_err(inode->i_sb,
-			  "inline data cross block boundary @ nid %llu",
-			  vi->nid);
-		DBG_BUGON(1);
-		return -EFSCORRUPTED;
-	}
 
 	memcpy(lnk, data + m_pofs, inode->i_size);
 	lnk[inode->i_size] = '\0';
