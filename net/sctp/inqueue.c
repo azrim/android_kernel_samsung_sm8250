e9e2a4504c889551ae0343d160bd87f4f7196a0f
Dmitry Antipov <dmantipov@yandex.ru>
2026-09-13 22:48:37 +0700

    UPSTREAM: net: sctp: fix skb leak in sctp_inq_free()
    In case of GSO, 'chunk->skb' pointer may point to an entry from
    fraglist created in 'sctp_packet_gso_append()'. To avoid freeing
    random fraglist entry (and so undefined behavior and/or memory
    leak), introduce 'sctp_inq_chunk_free()' helper to ensure that
    'chunk->skb' is set to 'chunk->head_skb' (i.e. fraglist head)
    before calling 'sctp_chunk_free()', and use the aforementioned
    helper in 'sctp_inq_pop()' as well.
    
    Reported-by: syzbot+8bb053b5d63595ab47db@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?id=0d8351bbe54fd04a492c2daab0164138db008042
    Fixes: 90017accff61 ("sctp: Add GSO support")
    Suggested-by: Xin Long <lucien.xin@gmail.com>
    Signed-off-by: Dmitry Antipov <dmantipov@yandex.ru>
    Acked-by: Xin Long <lucien.xin@gmail.com>
    Link: https://lore.kernel.org/r/20240214082224.10168-1-dmantipov@yandex.ru
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    (cherry picked from commit 4e45170d9acc2d5ae8f545bf3f2f67504a361338)
    (cherry picked from commit c4ef7e64a1e7ae1898a7d0b52ad5e68dc77c2f5e)

--- net/sctp/inqueue.c
+++ net/sctp/inqueue.c
@@ -53,6 +53,14 @@ void sctp_inq_init(struct sctp_inq *queue)
 	INIT_WORK(&queue->immediate, NULL);
 }
 
+/* Properly release the chunk which is being worked on. */
+static inline void sctp_inq_chunk_free(struct sctp_chunk *chunk)
+{
+	if (chunk->head_skb)
+		chunk->skb = chunk->head_skb;
+	sctp_chunk_free(chunk);
+}
+
 /* Release the memory associated with an SCTP inqueue.  */
 void sctp_inq_free(struct sctp_inq *queue)
 {
@@ -68,7 +76,7 @@ void sctp_inq_free(struct sctp_inq *queue)
 	 * free it as well.
 	 */
 	if (queue->in_progress) {
-		sctp_chunk_free(queue->in_progress);
+		sctp_inq_chunk_free(queue->in_progress);
 		queue->in_progress = NULL;
 	}
 }
@@ -145,9 +153,7 @@ struct sctp_chunk *sctp_inq_pop(struct sctp_inq *queue)
 				goto new_skb;
 			}
 
-			if (chunk->head_skb)
-				chunk->skb = chunk->head_skb;
-			sctp_chunk_free(chunk);
+			sctp_inq_chunk_free(chunk);
 			chunk = queue->in_progress = NULL;
 		} else {
 			/* Nothing to do. Next chunk in the packet, please. */
