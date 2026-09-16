0d915d1a66958b16bf6f42639070cc075f7c48f5
Arve Hjønnevåg <arve@android.com>
2026-09-16 21:56:01 +0700

    FROMLIST: sched/wait: Fix a kthread_park race with wait_woken()
    kthread_park and wait_woken have a similar race that kthread_stop and
    wait_woken used to have before it was fixed in
    cb6538e740d7543cd989128625cf8cac4b471e0a. Extend that fix to also cover
    kthread_park.
    
    Without this, a kthread that is parked while blocked in wait_woken() can
    miss the park request, call schedule_timeout() and stall until it times
    out instead of returning promptly.
    
    Bug: 274686101
    (cherry picked from commit ef73d6a4ef0b35524125c3cfc6deafc26a0c966a)

--- kernel/sched/wait.c
+++ kernel/sched/wait.c
@@ -389,9 +389,9 @@ int __sched autoremove_wake_function(struct wait_queue_entry *wq_entry,
 }
 EXPORT_SYMBOL(autoremove_wake_function);
 
-static inline bool is_kthread_should_stop(void)
+static inline bool is_kthread_should_stop_or_park(void)
 {
-	return (current->flags & PF_KTHREAD) && kthread_should_stop();
+	return (current->flags & PF_KTHREAD) && (kthread_should_stop() || kthread_should_park());
 }
 
 /*
@@ -424,7 +424,7 @@ long __sched wait_woken(struct wait_queue_entry *wq_entry, unsigned int mode,
 	 * or woken_wake_function() sees our store to current->state.
 	 */
 	set_current_state(mode); /* A */
-	if (!(wq_entry->flags & WQ_FLAG_WOKEN) && !is_kthread_should_stop())
+	if (!(wq_entry->flags & WQ_FLAG_WOKEN) && !is_kthread_should_stop_or_park())
 		timeout = schedule_timeout(timeout);
 	__set_current_state(TASK_RUNNING);
 
