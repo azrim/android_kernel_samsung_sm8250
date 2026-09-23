006344daab30af9c01f2e46b507422cb6488701c
azrim <mirzaspc@gmail.com>
2026-09-24 06:31:38 +0700

    sensors: a96t3x6: fix kfree of the firmware buffer pointer member
    The SDCARD firmware-load failure path did kfree(&data->firm_data_ums),
    freeing the address of the pointer member inside struct a96t3x6_data
    instead of the buffer: slab corruption plus a leak of the real allocation.
    Free the buffer itself and NULL the pointer, as the normal release path
    already does.  Same bug in all three a96t3x6 variants.

--- drivers/sensors/a96t3x6_wifi.c
+++ drivers/sensors/a96t3x6_wifi.c
@@ -2324,7 +2324,8 @@ static int a96t3x6_load_fw(struct a96t3x6_data *data, u8 cmd)
 	return ret;
 
 fail_sdcard_size:
-	kfree(&data->firm_data_ums);
+	kfree(data->firm_data_ums);
+	data->firm_data_ums = NULL;
 fail_sdcard_kzalloc:
 	filp_close(fp, current->files);
 fail_sdcard_open:
